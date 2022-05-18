a2d445beeebc970de53e799d7f88a326f4f9e74a
Luca Hagel <luca@luhagel.com>
2022-05-18 16:36:22 +0200

    OTP: Add child spec to top level yousign.ex

--- lib/yousign.ex
+++ lib/yousign.ex
@@ -2,4 +2,15 @@ defmodule Yousign do
   @moduledoc """
   Documentation for `Yousign`.
   """
+
+  def child_spec do
+    children = [
+      Yousign.API.child_spec()
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Yousign.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
