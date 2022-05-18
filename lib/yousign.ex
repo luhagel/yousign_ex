2897ac8acab64bc7da674ad65accac0f1d8caf9e
Luca Hagel <luca@luhagel.com>
2022-05-18 16:38:52 +0200

    OTP: Allow passing of opts to child_spec

--- lib/yousign.ex
+++ lib/yousign.ex
@@ -3,14 +3,14 @@ defmodule Yousign do
   Documentation for `Yousign`.
   """
 
-  def child_spec do
+  def child_spec(opts) do
     children = [
       Yousign.API.child_spec()
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Yousign.Supervisor]
+    opts = [strategy: :one_for_one, name: Yousign.Supervisor] ++ opts
     Supervisor.start_link(children, opts)
   end
 end
