8740bf922d03490266c2ce2410face9ab6387ca4
Luca Hagel <luca@luhagel.com>
2022-05-18 10:32:26 +0200

    Initial Commit

---
+++ lib/yousign_ex.ex
@@ -0,0 +1,18 @@
+defmodule YousignEx do
+  @moduledoc """
+  Documentation for `YousignEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YousignEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/yousign_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule YousignExTest do
+  use ExUnit.Case
+  doctest YousignEx
+
+  test "greets the world" do
+    assert YousignEx.hello() == :world
+  end
+end
