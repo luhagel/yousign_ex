e986cead1a5e3981edd87846c315f9286dcdbf4b
Luca Hagel <luca@luhagel.com>
2022-08-05 16:56:55 +0200

    Members: Expose get_signature_url/1 and :signature_ui_id config var

--- lib/yousign/api/members.ex
+++ lib/yousign/api/members.ex
@@ -1,5 +1,18 @@
 defmodule Yousign.API.Members do
+  import Yousign.API
   import Yousign.Request
 
+  alias Yousign.Config
+
   def list, do: make_request(:get, "members")
+
+  def get_signature_url(member_id) when is_binary(member_id) do
+    signature_ui_id = Config.resolve(:signature_ui_id, nil)
+
+    if is_binary(signature_ui_id) do
+      "#{get_web_url()}/procedure/sign?members=#{member_id}&signatureUi=/signature_uis/#{signature_ui_id}"
+    else
+      "#{get_web_url()}/procedure/sign?members=#{member_id}"
+    end
+  end
 end
