359f1f2ae70d653b40001d533778c1de0576b5dd
Luca Hagel <luca@luhagel.com>
2022-08-14 04:06:25 +0200

    Files: Add get/1

--- lib/yousign/api/files.ex
+++ lib/yousign/api/files.ex
@@ -7,6 +7,14 @@ defmodule Yousign.API.Files do
   def list, do: make_request(:get, "files")
 
   @doc """
+  Get a single file
+  """
+  def get(id) when is_binary(id) do
+    :get
+    |> make_request("/files/#{id}")
+  end
+
+  @doc """
   Creates a new file, accepts a payload built with build_file/2 or a custom one
   """
   def create(file) do
